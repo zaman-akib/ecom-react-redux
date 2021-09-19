34616c90691f1b5f10ee0825f117ca59c518e6c5
Akib Uz Zaman <akib.mist@gmail.com>
2021-09-20 01:21:49 +0600

    fixed routing issue

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import EcomApp from './components/ecom-app/EcomApp';
+import RouteHandler from './router/RouteHandler';
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <EcomApp />
+    <Provider store={store}>
+      <RouteHandler>
+        <EcomApp />
+      </RouteHandler>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
