9def6f5107829d4190a65a176aa6f912816835d2
Akib Uz Zaman <akib.zaman.04@gmail.com>
2021-09-22 13:17:01 +0600

    fixed checkout routing

--- src/router/RouteHandler.js
+++ src/router/RouteHandler.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import EcomApp from '../components/ecom-app/EcomApp'
 import NotFound from '../components/not-found/NotFound'
 import Checkout from '../components/cart/Checkout'
+import { useSelector } from 'react-redux'
 
 function RouteHandler() {
+    const cartItems = useSelector(state => state.cart.cartItems)
+
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={EcomApp}/>
-                <Route exact path='/checkout' component={Checkout}/>
+                <Route exact path='/checkout'>
+                    {cartItems.length === 0 ? (
+                        <Redirect to={{pathname: "/"}}/>
+                    ) : (
+                        <Checkout />
+                    )}
+                </Route>
                 <Route exact path='*' component={NotFound}/>
             </Switch>
         </BrowserRouter>
