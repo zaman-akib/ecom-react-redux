b805a5d7f33de48031685d0a546668666539e77e
Akib Uz Zaman <42386528+zaman-akib@users.noreply.github.com>
2021-09-26 10:46:58 +0600

    Revert "added image caching mechanism"

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react'
 import axios from 'axios'
 import { FETCH_PRODUCTS } from '../actionTypes/ProductActionTypes'
 import { useSelector } from 'react-redux'
-import imageToBase64 from "image-to-base64/browser"
 
 export const FetchProducts = (dispatch) => {
     const products = useSelector(state => state.fetchedProducts.products)
@@ -11,18 +10,6 @@ export const FetchProducts = (dispatch) => {
         if(products.length === 0){
             const fetchData = async () => {
                 const res = await axios.get('https://fakestoreapi.com/products')
-                    .then((responses) => {
-                        const { data } = responses
-
-                        data.map(d => {
-                            return imageToBase64(d.image).then((result) => {
-                                localStorage.setItem(d.id, result)
-                            })
-                        })
-
-                        return responses
-                    }
-                )
 
                 dispatch({
                     type: FETCH_PRODUCTS,
